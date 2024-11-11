56c1f76f332bde186a1f5dad1f5bfe0f52406375
SBHObject <168382040+SBHObject@users.noreply.github.com>
2024-11-12 00:17:15 +0900

    Update: Character 캐릭터기반에 컴포넌트 추가

--- IdleRPG_PersonalProject/Assets/Scripts/Character/Character.cs
+++ IdleRPG_PersonalProject/Assets/Scripts/Character/Character.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Character : MonoBehaviour
 {
+    public CharacterController Controller { get; private set; }
+    public NavMeshAgent Agent { get; private set; }
+
     [field: SerializeField] public CharacterSO Data {  get; private set; }
 
     private CharacterStateMachine stateMachine;
 
     private void Awake()
     {
+        Controller = GetComponent<CharacterController>();
+        Agent = GetComponent<NavMeshAgent>();
+
         stateMachine = new CharacterStateMachine(this);
     }
 
