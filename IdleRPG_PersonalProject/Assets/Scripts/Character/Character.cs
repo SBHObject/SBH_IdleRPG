40454e51fc93dd75468146c5b12445473c68859d
SBHObject <168382040+SBHObject@users.noreply.github.com>
2024-11-12 00:09:58 +0900

    Update: Character 상태 등록

--- IdleRPG_PersonalProject/Assets/Scripts/Character/Character.cs
+++ IdleRPG_PersonalProject/Assets/Scripts/Character/Character.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     [field: SerializeField] public CharacterSO Data {  get; private set; }
+
+    private CharacterStateMachine stateMachine;
+
+    private void Awake()
+    {
+        stateMachine = new CharacterStateMachine(this);
+    }
+
+    private void Start()
+    {
+        stateMachine.ChangeState(stateMachine.IdleState);
+    }
 }
--- IdleRPG_PersonalProject/Assets/Scripts/StateMachine/PlayableCharacterState/CharacterStateMachine.cs
+++ IdleRPG_PersonalProject/Assets/Scripts/StateMachine/PlayableCharacterState/CharacterStateMachine.cs
@@ -7,4 +7,18 @@ public class CharacterStateMachine : StateMachine
     public Character Character { get; }
 
     //╩Себ╣И
+    public CharacterIdleState IdleState { get; }
+    public CharacterAttackState AttackState { get; }
+    public CharacterMoveState MoveState { get; }
+    public CharacterDieState DieState { get; }
+
+    public CharacterStateMachine(Character character)
+    {
+        Character = character;
+
+        IdleState = new CharacterIdleState(this);
+        AttackState = new CharacterAttackState(this);
+        MoveState = new CharacterMoveState(this);
+        DieState = new CharacterDieState(this);
+    }
 }
